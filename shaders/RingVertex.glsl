1539c622bb9c5f214019e64fa1bac5f32dbfb7fb
Mario <91363514+Marwan997@users.noreply.github.com>
2023-07-05 16:51:00 +1200

    Update RingVertex.glsl
    better shaders

--- shaders/RingVertex.glsl
+++ shaders/RingVertex.glsl
@@ -2,7 +2,6 @@
 varying vec2 vUv;
 varying vec3 vNormal;
 varying vec3 vPosition;
-varying vec3 vPattern;
 
 uniform float uTime;
 
@@ -11,6 +10,14 @@ void main () {
     vNormal = normal;
     vPosition = position;
 
+    float stripesY = smoothstep(0.1, 0.5, mod(vUv.y * 70.0, 0.5));
+    float stripesX = sin(vUv.x * 1.0);
 
+    float strength = stripesY * stripesX;
+
+
+    vPosition.z += 1.5 - (sin(vPosition.y * strength + uTime * 0.4));
+
+    
     gl_Position = projectionMatrix * modelViewMatrix * vec4(vPosition, 1.0); 
 }
