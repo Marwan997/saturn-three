0a3befc5cfbb5e30c4a43632e06b77f7daa142bc
Mario <91363514+Marwan997@users.noreply.github.com>
2023-07-05 16:50:30 +1200

    Update RingFragment.glsl
    better shaders

--- shaders/RingFragment.glsl
+++ shaders/RingFragment.glsl
@@ -1,26 +1,28 @@
-
-
-varying vec3 vPosition;
 varying vec2 vUv;
 varying vec3 vNormal;
-varying vec3 vPattern;
-
-float noise(vec2 uv) {
-    return fract(sin(dot(uv, vec2(12.9898, 78.233))) * 43758.5453);
-}
+varying vec3 vPosition;
 
 void main() {
-    vec3 bcolor = vec3(1.0, 0.75, 0.6);
-    float line = atan(vUv.y) * 0.1 + 0.5;
-    float noiseOffset = noise(vUv.xy);
-    float displacement = line * noiseOffset * 0.05;
-    vec3 distortedPosition = vPosition + vNormal * displacement;
-    float stripe = mod(distortedPosition.z * 20.0, 0.85) * 2.5;
-    vec3 color = mix(bcolor, vec3(0.25), stripe);
-    float fresnel = 1.5 - dot(normalize(vNormal), normalize(vPosition));
-    vec3 glowColor = vec3(1.0, 0.59, 0.3);
-    float glowIntensity = 0.03;
-    color += glowColor * pow(fresnel, 3.5) * glowIntensity;
 
-    gl_FragColor = vec4(color, 0.6);
+    float stripesY =  mod(vUv.y* 56.0, 0.5);
+    float stripesX = 1.0 - sin(vUv.x * 0.01);
+
+    float strength = stripesY * stripesX;
+    vec2 newUv = vec2(stripesY,stripesX);
+    strength /= cnoise(newUv) * 5.1;
+
+    vec3 bcolor = vec3(0.98, 0.89, 0.75);
+    vec3 color = mix(bcolor, vec3(0.25), strength);
+
+    float fresnel1 =  1.0 - dot(vNormal, vPosition) * 0.3;
+    float fresnel2 =  1.0 - dot(vNormal, vPosition) * 0.5;
+    float fresnel = (fresnel1 * fresnel2) * 0.2;
+    color *= fresnel;
+
+    vec3 glowColor = vec3(0.9, 0.5, 0.5);
+    float glowIntensity = 0.01;
+    color += glowColor * pow(fresnel, 2.5) * glowIntensity;
+    color += bcolor * vec3(0.98,0.89,0.75);
+    
+    gl_FragColor = vec4(color, 0.5);
 }
