16a3861fb03f9f70ebec3236338fba94cc07b1a8
Mario <91363514+Marwan997@users.noreply.github.com>
2023-07-05 16:51:22 +1200

    Update SaturnFragment.glsl
    better shaders

--- shaders/SaturnFragment.glsl
+++ shaders/SaturnFragment.glsl
@@ -3,8 +3,7 @@ varying vec3 vNormal;
 varying vec2 vUv;
 
 void main() {
-    float intensity = normalize(1.0- dot(vNormal, vec3(0.0,0.0,1.0)));
-    vec3 atmosphere = vec3(0.7, 0.6, 0.4) * pow(intensity, 1.2);
+    vec3 atmosphere = vec3(1.0, 0.91, 0.95);
     gl_FragColor = vec4(atmosphere*texture2D(saturnTexture, vUv).xyz , 1.0);
 }
 
