ac37e257c82ebeb4c5404a1fc4c84f70be6d14b7
Mario <91363514+Marwan997@users.noreply.github.com>
2023-07-05 16:51:41 +1200

    Update SaturnGlowFragment.glsl

--- shaders/SaturnGlowFragment.glsl
+++ shaders/SaturnGlowFragment.glsl
@@ -4,9 +4,9 @@ varying vec3 vPosition;
 void main() {
 
     vec3 viewDirection = normalize(cameraPosition - vPosition);
-    float fresnel = 0.02 - pow(1.0 - abs(dot(viewDirection, vNormal)), 1.4);
+    float fresnel = 0.01 - pow(1.0 - abs(dot(viewDirection, vNormal)), 0.3);
 
-    vec3 atmosphere = vec3(0.7, 0.64, 0.3);
+    vec3 atmosphere = vec3(1.0, 0.95, 0.85);
 
     gl_FragColor = vec4(vec3(fresnel) + atmosphere, 1.0);
 }
